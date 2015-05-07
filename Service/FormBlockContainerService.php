75c277295f4dc9aa6323cfdac3293bc075347845
Christopher West <9ninemind@gmail.com>
2015-05-07 15:31:40 -0600

    adds FormBlockContainerService

---
+++ Service/FormBlockContainerService.php
@@ -0,0 +1,53 @@
+<?php
+namespace BlackBoxCode\Pando\Bundle\ContentBundle\Service;
+
+use Sonata\BlockBundle\Model\BlockInterface;
+use Symfony\Component\Form\FormInterface;
+
+class FormBlockContainerService
+{
+    /** @var FormInterface */
+    private $form;
+
+    /** @var BlockInterface */
+    private $blockInterface;
+
+
+    /**
+     * @return FormInterface
+     */
+    public function getForm()
+    {
+        return $this->form;
+    }
+
+    /**
+     * @param FormInterface $form
+     * @return $this
+     */
+    public function setForm(FormInterface $form)
+    {
+        $this->form = $form;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBlockInterface()
+    {
+        return $this->blockInterface;
+    }
+
+    /**
+     * @param BlockInterface $blockInterface
+     * @return $this
+     */
+    public function setBlockInterface(BlockInterface $blockInterface)
+    {
+        $this->blockInterface = $blockInterface;
+
+        return $this;
+    }
+}
