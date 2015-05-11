ce1f855f11080fb047754326b5816a4fb1ce138c
Christopher West <9ninemind@gmail.com>
2015-05-11 15:14:13 -0600

    adds AssetFactory test

--- Document/Stylesheet.php
+++ Document/Stylesheet.php
@@ -2,7 +2,6 @@
 namespace BlackBoxCode\Pando\Bundle\ContentBundle\Document;
 
 use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCR;
-use Symfony\Cmf\Bundle\MediaBundle\Doctrine\Phpcr\File;
 
 /**
  * @PHPCR\Document(referenceable=true)
---
+++ Tests/Factory/AssetFactoryTest.php
@@ -0,0 +1,94 @@
+<?php
+namespace BlackBoxCode\Pando\Bundle\ContentBundle\Tests\Service;
+
+use BlackBoxCode\Pando\Bundle\ContentBundle\Document\File;
+use BlackBoxCode\Pando\Bundle\ContentBundle\Document\Image;
+use BlackBoxCode\Pando\Bundle\ContentBundle\Document\Javascript;
+use BlackBoxCode\Pando\Bundle\ContentBundle\Document\Stylesheet;
+use BlackBoxCode\Pando\Bundle\ContentBundle\Factory\AssetFactory;
+
+class AssetFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /** \PHPUnit_Framework_MockObject_MockObject|\AssetFactory */
+    private $mAssetFactory;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject|\SplFileInfo */
+    private $mSplFileInfo;
+
+
+    public function setUp()
+    {
+        $this->mAssetFactory = $this->getMock('BlackBoxCode\Pando\Bundle\ContentBundle\Factory\AssetFactory', ['getMimeType']);
+        $this->mSplFileInfo = $this
+            ->getMockBuilder('\SplFileInfo')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+    }
+
+    /**
+     * @test
+     */
+    public function create_image()
+    {
+        $this->mSplFileInfo
+            ->expects($this->once())
+            ->method('getExtension')
+            ->willReturn('png')
+        ;
+
+        $this->mAssetFactory
+            ->expects($this->once())
+            ->method('getMimeType')
+            ->willReturn('image/png')
+        ;
+
+        $return = $this->mAssetFactory->create($this->mSplFileInfo);
+        $this->assertInstanceOf(get_class(new Image()), $return);
+    }
+
+    /**
+     * @test
+     */
+    public function create_stylesheet()
+    {
+        $this->mSplFileInfo
+            ->expects($this->once())
+            ->method('getExtension')
+            ->willReturn('css')
+        ;
+
+        $return = $this->mAssetFactory->create($this->mSplFileInfo);
+        $this->assertInstanceOf(get_class(new Stylesheet()), $return);
+    }
+
+    /**
+     * @test
+     */
+    public function create_javascript()
+    {
+        $this->mSplFileInfo
+            ->expects($this->once())
+            ->method('getExtension')
+            ->willReturn('js')
+        ;
+
+        $return = $this->mAssetFactory->create($this->mSplFileInfo);
+        $this->assertInstanceOf(get_class(new Javascript()), $return);
+    }
+
+    /**
+     * @test
+     */
+    public function create_other()
+    {
+        $this->mSplFileInfo
+            ->expects($this->once())
+            ->method('getExtension')
+            ->willReturn('somethingelse')
+        ;
+
+        $return = $this->mAssetFactory->create($this->mSplFileInfo);
+        $this->assertInstanceOf(get_class(new File()), $return);
+    }
+}
